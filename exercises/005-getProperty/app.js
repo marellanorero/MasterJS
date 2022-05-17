d33a961e023621aa079fc8b3885c0b88444299e7
marellanorero <marellanonorero@gmail.com>
2022-05-17 23:22:46 +0000

    avanzando

--- exercises/005-getProperty/app.js
+++ exercises/005-getProperty/app.js
@@ -1,3 +1,16 @@
 function getProperty(obj, key) {
   // your code here
+  for(key in obj){
+    if(obj[key]){
+      return obj[ket];
+    } else {
+      return undefined;
+    }
+  }
 }
+
+var car = {
+  model: 'Toyota'
+};
+var output = getProperty(car, 'model');
+console.log(output); // --> 'Toyota'
