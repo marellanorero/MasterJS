56df369936334a684e62acaa321f6f8989772e63
marellanorero <marellanonorero@gmail.com>
2022-05-28 22:56:18 +0000

    avanzando

--- exercises/006-addProperty/app.js
+++ exercises/006-addProperty/app.js
@@ -1,3 +1,10 @@
 function addProperty(obj, key) {
   // your code here
+  if(key in obj){
+    return obj[key];
+  } 
 }
+
+var myObj = {};
+addProperty(myObj, 'myProperty');
+console.log(myObj.myProperty); // --> true
--- exercises/007-removeProperty/app.js
+++ exercises/007-removeProperty/app.js
@@ -1,3 +1,15 @@
 function removeProperty(obj, key) {
   // your code here
+  if(key in obj){
+    return delete obj[key]
+  } else {
+    return obj[key]
+  }
 }
+
+var obj = {
+  name: 'Sam',
+  age: 20
+}
+removeProperty(obj, 'name');
+console.log(obj.name); // --> undefined
